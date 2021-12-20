50140319a2e7823b39ececa24afef6800706657a
Rodrigo Pires <digaomilleniun@gmail.com>
2021-12-19 21:41:23 -0300

    Adição de execução de todos os testes do módulo 25

---
+++ mod25/ExemploVendasMod25/src/test/java/br/com/rpires/AllTests.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package br.com.rpires;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * @author rodrigo.pires
+ *
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ ClienteServiceTest.class, ClienteDAOTest.class,
+	ProdutoServiceTest.class, ProdutoDAOTest.class,
+	VendaDAOTest.class})
+public class AllTests {
+
+}
