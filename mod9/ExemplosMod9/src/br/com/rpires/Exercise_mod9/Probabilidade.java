903661ba7a35beb57bf66ccaa96e13b8c9464436
Steve <falecomstevevaz@gmail.com>
2023-09-09 14:27:41 -0300

    Some changes in the probability.java file

--- mod9/ExemplosMod9/src/br/com/rpires/Exercise_mod9/Probabilidade.java
+++ mod9/ExemplosMod9/src/br/com/rpires/Exercise_mod9/Probabilidade.java
@@ -2,12 +2,12 @@ package Tests_java_bySteve;
 
 public class Probabilidade {
     public static void main(String[] args) {
-        int totalNumeros = 25;
-        int numerosParaAcertar = 15;
+        Integer totalNumeros = 25;
+        Integer numerosParaAcertar = 15;
 
-        double probabilidadeDeNaoAcertar = Math.pow((double)(totalNumeros - 1) / totalNumeros, numerosParaAcertar);
+        Double probabilidadeDeNaoAcertar = Math.pow((double)(totalNumeros - 1) / totalNumeros, numerosParaAcertar);
 
-        double probabilidadeDeAcertarPeloMenosUm = 1 - probabilidadeDeNaoAcertar;
+        Double probabilidadeDeAcertarPeloMenosUm = 1 - probabilidadeDeNaoAcertar;
 
         System.out.println("Probabilidade de pelo menos uma pessoa acertar os 15 números: " + probabilidadeDeAcertarPeloMenosUm);
     }
