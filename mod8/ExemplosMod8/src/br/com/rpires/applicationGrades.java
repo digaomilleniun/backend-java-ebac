790e85bea0ca862b17b3be459b39f65f21866564
Steve <falecomstevevaz@gmail.com>
2023-09-09 17:57:59 -0300

    Changing the calculation of grades.

---
+++ mod8/ExemplosMod8/src/br/com/rpires/applicationGrades.java
@@ -0,0 +1,37 @@
+package exercise_module8;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Grades;
+
+/*
+* @author: Steve Vaz
+*/
+public class applicationGrades {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Grades student = new Grades();
+		
+	    student.name = sc.nextLine();
+	    student.grade1 = sc.nextDouble();
+	    student.grade2 = sc.nextDouble();
+	    student.grade3 = sc.nextDouble();
+	    student.grade4 = sc.nextDouble();
+	    
+	    System.out.printf("Final Grade: %.2f%n", student.finalGrade());
+	    System.out.printf("Grade average: %.2f%n", student.mediaOfGrades());
+	    
+	    if (student.finalGrade() < 32.00) {
+	    	System.out.println("Disapproved");
+	    } else {
+	    	System.out.println("Mission complete");
+	    }
+	    
+	    sc.close();
+	}
+}
