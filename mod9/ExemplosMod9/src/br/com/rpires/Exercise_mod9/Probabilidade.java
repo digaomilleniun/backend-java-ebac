cee4fa8ad65138ca2d43763201f3f8fbb7379436
Steve <falecomstevevaz@gmail.com>
2023-09-10 11:33:20 -0300

    Printing the primitive and wrapper type

--- mod9/ExemplosMod9/src/br/com/rpires/Exercise_mod9/Probabilidade.java
+++ mod9/ExemplosMod9/src/br/com/rpires/Exercise_mod9/Probabilidade.java
@@ -8,7 +8,13 @@ public class Probabilidade {
         double probabilidadeDeNaoAcertar = Math.pow(Double.valueOf(totalNumeros - 1) / totalNumeros, numerosParaAcertar);
 
         double probabilidadeDeAcertarPeloMenosUm = Double.valueOf(1 - probabilidadeDeNaoAcertar);
+
+        // Imprimindo a variável probabilidadeDeAcertarPeloMenosUm no tipo primitivo double
+        System.out.println("Primitivo: Probabilidade de pelo menos uma pessoa acertar os 15 números: " + probabilidadeDeAcertarPeloMenosUm);
         
-        System.out.println("Probabilidade de pelo menos uma pessoa acertar os 15 números: " + probabilidadeDeAcertarPeloMenosUm);
+        System.out.println();
+        
+        // Imprimindo a variável probabilidadeDeAcertarPeloMenosUm no tipo wrapper Double
+        System.out.println("Wrapper: Probabilidade de pelo menos uma pessoa acertar os 15 números: " + Double.valueOf(probabilidadeDeAcertarPeloMenosUm));
     }
 }
