9f1ccd1cf72ac5f4fc860baaad82b25d3d019075
Steve <falecomstevevaz@gmail.com>
2023-09-10 11:36:12 -0300

    Writing some more.

--- mod9/ExemplosMod9/src/br/com/rpires/Exercise_mod9/Probabilidade.java
+++ mod9/ExemplosMod9/src/br/com/rpires/Exercise_mod9/Probabilidade.java
@@ -10,11 +10,11 @@ public class Probabilidade {
         double probabilidadeDeAcertarPeloMenosUm = Double.valueOf(1 - probabilidadeDeNaoAcertar);
 
         // Imprimindo a variável probabilidadeDeAcertarPeloMenosUm no tipo primitivo double
-        System.out.println("Primitivo: Probabilidade de pelo menos uma pessoa acertar os 15 números: " + probabilidadeDeAcertarPeloMenosUm);
+        System.out.println("Primitivo double: Probabilidade de pelo menos uma pessoa acertar os 15 números: " + probabilidadeDeAcertarPeloMenosUm);
         
         System.out.println();
         
         // Imprimindo a variável probabilidadeDeAcertarPeloMenosUm no tipo wrapper Double
-        System.out.println("Wrapper: Probabilidade de pelo menos uma pessoa acertar os 15 números: " + Double.valueOf(probabilidadeDeAcertarPeloMenosUm));
+        System.out.println("Wrapper Double: Probabilidade de pelo menos uma pessoa acertar os 15 números: " + Double.valueOf(probabilidadeDeAcertarPeloMenosUm));
     }
 }
